3553dd57e377caed587233b2510c693116b93fea
Kavishka15055 <kavishkapiyumal22@gmail.com>
2025-10-14 19:31:36 +0530

    Update firstTest.js

--- test/firstTest.js
+++ test/firstTest.js
@@ -2,16 +2,17 @@ const { Builder, Browser, By, Key, until } = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 const chromedriver = require('chromedriver');
 
-;(async function example() {
+(async function example() {
   const service = new chrome.ServiceBuilder(chromedriver.path);
   let driver = await new Builder()
     .forBrowser(Browser.CHROME)
     .setChromeService(service)
     .build();
   try {
-    await driver.get('https://www.google.com/ncr')
-    await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN)
-    await driver.wait(until.titleIs('webdriver - Google Search'), 30000) // Increased timeout to 10 seconds
+    await driver.navigate().to('https://www.google.com/ncr');
+    await driver.sleep(5000)
+    await driver.navigate().refresh()
+    
   } finally {
     await driver.quit()
   }
